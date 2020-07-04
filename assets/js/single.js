b499b93ab6ff201ff771821b5a68213e4863d613
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-07-04 00:30:54 -0700

    add error handlers

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -17,12 +18,26 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+           document.location.replace("./index.html");
         }
     });
 };
 
-getRepoIssues("expressjs/express");
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -71,3 +86,7 @@ var displayWarning = function(repo) {
     //append to warning container
     limitWarningEl.appendChild(linkEl);
 }
+
+getRepoIssues();
+getRepoName();
+
